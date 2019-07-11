61ad01d90b60e43902efb5f27c5d637b22a0f5c6
ioir123ju <549981178@qq.com>
2019-07-11 16:55:07 +0800

    [CHG] 1.增加voc的训练和预测

--- tools/voc_detect.py
+++ tools/voc_detect.py
@@ -2,12 +2,12 @@
 # -*- encoding: utf-8 -*-
 '''
 @File    :   voc_detect.py    
-@Contact :   JZ
-@License :   (C)Copyright 2018-2019, JZ All rights reserved.
+@Contact :   juzheng@hxdi.com
+@License :   (C)Copyright 2018-2019, Liugroup-NLPR-CASIA
 
 @Modify Time      @Author    @Version    @Desciption
 ------------      -------    --------    -----------
-2019/7/11 14:10   JZ      1.0         None
+2019/7/11 14:10   juzheng      1.0         None
 '''
 
 
@@ -17,8 +17,6 @@ import os
 import cv2
 import numpy as np
 import os
-from torch2trt import torch2trt
-import torch
 
 def listDir(path, list_name):
     """
@@ -35,41 +33,21 @@ def listDir(path, list_name):
 
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-cfg = mmcv.Config.fromfile('configs/reppoints/voc_reppoints_moment_r50_fpn_2x_mt.py')
+cfg = mmcv.Config.fromfile('configs/voc_faster_rcnn_r50_fpn_1x.py')
 cfg.model.pretrained = None
 
 # YangHE/mmdetection-master/work_dirs/faster_rcnn_r50_fpn_1x_voc0712/epoch_1.pth
 # construct the model and load checkpoint
 
-model = init_detector(cfg, 'work_dirs/reppoints_moment_r50_fpn_2x_mt/latest.pth', device='cuda:0')
-x = torch.ones((1, 3, 800, 800)).cuda()
-cap = cv2.VideoCapture("/home/juzheng/dataset/granary/29-1.avi")
-i = 0
-while True:
-
-    res, image1 = cap.read()
-    i += 1
-    if i % 15 != 0:
-        continue
-    image1 = cv2.resize(image1, (1024, 600))
-    result = inference_detector(model, image1)
-
-    show_result(image1, result, model.CLASSES, out_file='result/mask/result_{}.jpg'.format(i))
+model = init_detector(cfg, 'work_dirs/faster_rcnn_r50_fpn_1x/latest.pth', device='cuda:0')
 
 
-
+path = "data/my_grain"
+# path = "data/VOCdevkit/VOC2007/JPEGImages"
 imgs = []
-path = "data/VOCdevkit/VOC2007/JPEGImages"
-with open('data/VOCdevkit/VOC2007/ImageSets/Main/test.txt', 'r') as file:
-    for line in file:
-        imgs.append(os.path.join(path, line.rstrip('\n') + ".jpg"))
-
-# listDir(path, imgs)
-for i, img in enumerate(imgs):
-    result = inference_detector(model, img)
-    show_result(img, result, model.CLASSES, out_file='result/mask/result_{}.jpg'.format(i))
-# for i, result in enumerate(inference_detector(model, imgs)):
-#     show_result(imgs[i], result, model.CLASSES, out_file='result/mask/result_{}.jpg'.format(i))
+listDir(path, imgs)
+for i, result in enumerate(inference_detector(model, imgs)):
+    show_result(imgs[i], result, model.CLASSES, out_file='result/grain/result_{}.jpg'.format(i))
 # print(result[0].shape)
 #
 # img = cv2.imread('test.jpg')
@@ -85,43 +63,3 @@ for i, img in enumerate(imgs):
 # cv2.imshow("s", img)
 # cv2.waitKey(0)
 
-#
-# from mmdet.datasets import dali_transforms
-# import time
-#
-#
-# def test_dali():
-#     # eii = ExternalInputIterator(batch_size)
-#     # iterator = iter(eii)
-#
-#     os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-#     cfg = mmcv.Config.fromfile('config/voc_2_libra_faster_rcnn_r50_fpn_1x.py')
-#     cfg.model.pretrained = None
-#     # granary_model = init_detector(cfg, 'model/libra.pth', device='cuda:0')
-#     person_model = init_detector(cfg, 'model/libra_person.pth', device='cuda:0')
-#     ann_file = cfg.data.test.ann_file
-#     image_ids = mmcv.list_from_file(ann_file)
-#     img_prefix = cfg.data.test.img_prefix
-#     batch_size = 32
-#
-#     num = len(image_ids)
-#     eii = dali_transforms.ExternalInputIterator(img_prefix, ann_file, batch_size, device_id=0, num_gpus=1)
-#     pipe = dali_transforms.ExternalSourcePipeline(batch_size=batch_size, num_threads=2, device_id=0, external_data=eii,
-#                                                   cfg=cfg.img_norm_cfg)
-#     pipe.build()
-#     start = time.clock()
-#
-#     for i in range(num // batch_size + 1):
-#         try:
-#             images, labels = pipe.run()
-#             print('frame :', len(images))
-#
-#         except Exception as e:
-#             print(e)
-#             # cv2.imshow("{}".format(image_id), imageout1)
-#             # cv2.waitKey()
-#             # pass
-#             # cv2.destroyWindow("{}".format(image_id))
-#             break
-#     elapsed = (time.clock() - start)
-#     print('NUM:{}, TIME USE:{}'.format(num, elapsed))
