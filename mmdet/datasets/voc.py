216fa86f6c0cf3a16477b87da0eb72db9f8b6cff
ioir123ju <549981178@qq.com>
2019-07-11 16:55:07 +0800

    [CHG] 1.增加voc的训练和预测

--- mmdet/datasets/voc.py
+++ mmdet/datasets/voc.py
@@ -1,14 +1,9 @@
-from .registry import DATASETS
 from .xml_style import XMLDataset
 
 
-@DATASETS.register_module
 class VOCDataset(XMLDataset):
 
-    CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car',
-               'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse',
-               'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train',
-               'tvmonitor')
+    CLASSES = ('door','hline','gline')
 
     def __init__(self, **kwargs):
         super(VOCDataset, self).__init__(**kwargs)
