6e626f23c6c98a2b2c634c3c35e378a667d90631
Kai Chen <chenkaidev@gmail.com>
2019-12-25 12:54:49 +0800

    add NASFPN (#1246)
    * add NASFPN
    
    * minor fixes
    
    * fix downsample, add norm to lateral
    
    * update downsample and configs
    
    * remove additional blank line
    
    * update docs
    
    * add benchmark
    
    * minor fix
    
    Co-authored-by: Cao Yuhang <yhcao6@gmail.com>

--- mmdet/models/anchor_heads/__init__.py
+++ mmdet/models/anchor_heads/__init__.py
@@ -7,12 +7,12 @@ from .ga_rpn_head import GARPNHead
 from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
 from .reppoints_head import RepPointsHead
 from .retina_head import RetinaHead
+from .retina_sepbn_head import RetinaSepBNHead
 from .rpn_head import RPNHead
 from .ssd_head import SSDHead
-from .ttf_head import TTFHead
 
 __all__ = [
     'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead',
-    'GARPNHead', 'RetinaHead', 'GARetinaHead', 'SSDHead', 'FCOSHead',
-    'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead', 'TTFHead'
+    'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
+    'FCOSHead', 'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead'
 ]
